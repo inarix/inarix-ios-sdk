fa351aa0b412e5367da28c081201fb4b66957e0c
Guillaume Robin <guillaume@inarix.com>
2024-08-29 16:11:01 +0200

    Update binary targeT

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
                 .product(name: "PostHog", package: "posthog-ios"),
                 .target(name: "InarixSDK"),
             ]),
-        .binaryTarget(name: "InarixSDK", url: "https://storage.googleapis.com/prod-inarix-public/dev/jpp/InarixSDK.xcframework.zip", checksum: "5f2b20c09e879936d7b4bf7c56f5a4e83c20cf90b8e0ef29883323c1ea220638"),
+        .binaryTarget(name: "InarixSDK", url: "https://storage.googleapis.com/prod-inarix-public/dev/InarixSDK_v0.1.0.xcframework.zip", checksum: "5627ff65f83c25d8ebc6a1a34e726516eecc6dc1e1ba767e9d9f778f2293db7e"),
         .testTarget(
             name: "inarix-ios-sdkTests",
             dependencies: ["inarix-ios-sdk"]),
