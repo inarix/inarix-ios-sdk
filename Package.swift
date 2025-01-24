122e03e35c049a0dfdd2b2ffd4311f4dcec7b436
Guillaume Robin <guillaume@inarix.com>
2025-01-24 09:27:08 +0100

    (fix): v0.2.5-alpha dependencies fixing issue with Posthog

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Add dependencies if your SDK uses other packages.
         .package(url: "https://github.com/newrelic/newrelic-ios-agent-spm.git", branch: "main"),
-        .package(url: "https://github.com/PostHog/posthog-ios.git", from: "3.0.0"),
+        .package(url: "https://github.com/PostHog/posthog-ios.git", branch: "fix/internal-phlibwebp"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
