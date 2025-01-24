d01db26cc7bab92490af65bdb639d2b743ef024a
Guillaume Robin <guillaume@inarix.com>
2025-01-24 13:41:54 +0100

    More trying stuff

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
                 .product(name: "PostHog", package: "posthog-ios"),
                 .target(name: "InarixSDK"),
             ]),
-        .binaryTarget(name: "InarixSDK", url: "https://storage.googleapis.com/prod-inarix-public/dev/InarixSDK_v0.2.7-alpha.xcframework.zip", checksum: "4ce7679e65c75bf97fac73920994db415cf3532cd174c21acb1e611416d9f5f7"),
+        .binaryTarget(name: "InarixSDK", url: "https://storage.googleapis.com/prod-inarix-public/dev/InarixSDK_v0.2.8-alpha.xcframework.zip", checksum: "33219cf7fafc973854138a13e933bfa96953e2e0a8b5e06ae68f554d12fa4683"),
         .testTarget(
             name: "inarix-ios-sdkTests",
             dependencies: ["inarix-ios-sdk"]),
