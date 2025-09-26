72ad41550df3f648c43a82b981eb07a6f73d19a1
Inarix Infra <infra@inarix.com>
2025-09-26 18:54:27 +0200

    Release 0.6.8-beta

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
                 .product(name: "PostHog", package: "posthog-ios"),
                 .target(name: "InarixSDK"),
             ]),
-        .binaryTarget(name: "InarixSDK", url: "https://storage.googleapis.com/prod-inarix-public/dev/InarixSDK_v0.6.5-beta.xcframework.zip", checksum: "9e833b61003286e571ecd11b50df18aad6d20bc2bb594e579dde08b73fbffec5"),
+        .binaryTarget(name: "InarixSDK", url: "https://storage.googleapis.com/prod-inarix-public/dev/InarixSDK_v0.6.8-beta.xcframework.zip", checksum: "5fbad0d8852eb59a0f4ab0eece932e92332d4f52bc6ceb8fd7e014be121fdd99"),
         .testTarget(
             name: "inarix-ios-sdkTests",
             dependencies: ["inarix-ios-sdk"]),
