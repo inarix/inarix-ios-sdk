195019731bafa2a17c08c491500b34a8d8f81424
Youbaraj POUDEL <youbaraj@inarix.com>
2026-06-12 15:13:48 +0200

    crash reporter fixed

--- Package.swift
+++ Package.swift
@@ -15,7 +15,9 @@ let package = Package(
     dependencies: [
         // Add dependencies if your SDK uses other packages.
         .package(url: "https://github.com/newrelic/newrelic-ios-agent-spm.git", branch: "main"),
-        .package(url: "https://github.com/PostHog/posthog-ios.git", from: "3.48.2"),
+        .package(url: "https://github.com/PostHog/posthog-ios.git", from: "3.48.3"),
+        // PostHog pulls CrashReporter; linking it on this target fixes "Missing required module 'CrashReporter'" for apps that depend on inarix-ios-sdk.
+        .package(url: "https://github.com/microsoft/plcrashreporter.git", from: "1.12.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -25,6 +27,7 @@ let package = Package(
             dependencies: [
                 .product(name: "NewRelic", package: "newrelic-ios-agent-spm"),
                 .product(name: "PostHog", package: "posthog-ios"),
+                .product(name: "CrashReporter", package: "plcrashreporter"),
                 .target(name: "InarixSDK"),
             ]),
         .binaryTarget(name: "InarixSDK", url: "https://storage.googleapis.com/prod-inarix-public/test/InarixSDK_v1.0.4-test.xcframework.zip", checksum: "232b148f3af5ef9cef19285c70a57ccddd69061f97a5cd826846faabfb6caa4a"),
